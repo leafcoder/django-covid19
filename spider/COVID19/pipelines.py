2e6549339f7a73e4b9d67b911617e446b5a9af13
leafcoder <leafcoder@gmail.com>
2020-04-11 16:15:01 +0800

    feat: add json files for city/province/country

--- spider/COVID19/pipelines.py
+++ spider/COVID19/pipelines.py
@@ -5,8 +5,12 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import os
 import json
 import sqlite3
+from posixpath import join, exists
+from django.conf import settings
+from django.forms.models import model_to_dict
 from .items import CityItem, ProvinceItem, CountryItem
 
 class Covid19Pipeline(object):
@@ -34,3 +38,28 @@ class Covid19Pipeline(object):
                 countryShortCode=item['countryShortCode'],
                 defaults=item)
             return item
+
+    def close_spider(self,spider):
+        basedir = join(settings.BASE_DIR, 'spider', 'data')
+        if not exists(basedir):
+            os.makedirs(basedir)
+
+        data = []
+        for inst in CityItem.django_model.objects.all():
+            data.append(model_to_dict(inst))
+        with open(join(basedir, 'city.json'), 'w') as f:
+            json.dump(data, f, ensure_ascii=False)
+
+
+        data = []
+        for inst in ProvinceItem.django_model.objects.all():
+            data.append(model_to_dict(inst))
+        with open(join(basedir, 'province.json'), 'w') as f:
+            json.dump(data, f, ensure_ascii=False)
+
+
+        data = []
+        for inst in CountryItem.django_model.objects.all():
+            data.append(model_to_dict(inst))
+        with open(join(basedir, 'country.json'), 'w') as f:
+            json.dump(data, f, ensure_ascii=False)
